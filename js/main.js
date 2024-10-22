2a49f522e825af5d9ce7b413763f8235f007db49
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-10-22 15:39:52 -0500

    feat: :sparkles: Agregar código

--- js/main.js
+++ js/main.js
@@ -7,22 +7,31 @@ function todayWeather() {
 
         if(this.readyState === 4 && this.status === 200){
             let response = JSON.parse(this.responseText);
+
+            document.getElementById("iconWeather").setAttribute("href", response.current.condition.icon)
+
             document.getElementById("temperature_main").innerText = `${response.current.temp_c}°`
+            document.getElementById("feels").innerText=`Feels like ${response.current.feelslike_c}°`
+
             document.getElementById("location_place").innerText = `${response.location.name}, ${response.location.country}`
             
             document.getElementById("weather_icon").removeAttribute("src");
             document.getElementById("weather_icon").setAttribute("src", response.current.condition.icon);
-            
             document.getElementById("weather_text").innerText=`${response.current.condition.text}`
             
-            document.getElementById("feels").innerText=`Feels like ${response.current.feelslike_c}°`
+
+            document.getElementById("wind").innerText=`${response.current.wind_kph} km/h`
+            document.getElementById("uv").innerText=`${response.current.uv}`
+            document.getElementById("rain").innerText=`${response.current.cloud}%`
+            document.getElementById("pressure").innerText=`${response.current.pressure_mb}hpa`
 
             time = response.location.localtime
             datetime(time)
 
+            weatherChange(time,response.current)
         }else if(this.readyState == 4){
             console.log("Error :(",this.statusText)
-
+            alert("Couldn't connect to server...")
         }
     }
     xhr.send();
@@ -51,3 +60,40 @@ function datetime(time) {
 }
 
 todayWeather()
+
+function tomorrowWeather(){}
+
+function tenForecast(){}
+
+function weatherChange(date,current){
+
+    let today = date.slice(0,10)
+    let todayTime = new Date(today).getTime();
+
+    let day =1*24*60*60;
+
+    let yesterday = todayTime-day
+    console.log(yesterday)
+    yesterday = new Date(yesterday).toISOString().slice(0,10);
+    console.log(yesterday)
+
+    let xhr = new XMLHttpRequest();
+    urlYesterday=`https://api.weatherapi.com/v1/history.json?key=01126ba1df5a4d99aa2123911242110&q=Bucaramanga&dt=${yesterday}&aqi=no`
+    xhr.open("GET",urlYesterday,true);
+
+    xhr.onreadystatechange = function(){
+        if(this.readyState === 4 && this.status === 200){
+            let response = JSON.parse(this.responseText);
+
+            document.getElementById("windChange").innerText=`${((response.forecast.forecastday[0].day.maxwind_kph)-(current.wind_kph)).toFixed()} km/h`
+            document.getElementById("uvChange").innerText=`${((response.forecast.forecastday[0].day.uv)-(current.wind_kph)).toFixed()}`
+            document.getElementById("rainChange").innerText=`${((response.forecast.forecastday[0].day.daily_chance_of_rain)-(current.cloud)).toFixed()}%`
+            document.getElementById("pressureChange").innerText=`${(1027-current.pressure_mb).toFixed()}hpa`
+
+        }else if(this.readyState == 4){
+            console.log("Error :(",this.statusText)
+            alert("Couldn't connect to server...")
+        }
+    }
+    xhr.send();
+}
